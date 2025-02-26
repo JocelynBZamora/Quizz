39405251dbefb472d3352a3fa98e6ff8f242ac7b
Jocelyn Berenice Zamora De La Cerda <201G0293@rcarbonifera.tecnm.mx>
2025-02-25 22:08:11 -0600

    .

---
+++ Quizz/Model/PersonasDTO.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace QuizzServer.Model
+{
+    public partial class PersonasDTO : ObservableObject
+    {
+        [ObservableProperty]
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        //cliente
+        [JsonIgnore]
+        public TcpClient? Cliente { get; set; }
+    }
+  
+}
---
+++ Quizz/Model/PreguntasModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizzServer.Model
+{
+    public class PreguntasModel
+    {
+        public int Id { get; set; }
+        public string Pregunta { get; set; } = null!;
+        public string Respuesta1 { get; set; } = null!;
+        public string Respuesta2 { get; set; } = null!;
+        public string Respuesta3 { get; set; } = null!;
+        public string Respuesta4 { get; set; } = null!;
+        public string RCorrecta { get; set; } = null!;
+        
+    }
+
+}
