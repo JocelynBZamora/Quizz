14c8b2b7bf33df956b8410e4f06a324997d1e1ef
Jocelyn Berenice Zamora De La Cerda <201G0293@rcarbonifera.tecnm.mx>
2025-03-02 23:41:34 -0600

    bdd 2 secciones

--- Quizz/Model/DatosModel.cs
+++ Quizz/Model/DatosModel.cs
@@ -12,7 +12,7 @@ namespace QuizzServer.Model
     {
 
         //crea la bdd
-        static string p = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Quizz-2.db");
+        static string p = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Quizz-1.5.db");
         SQLiteConnection conn = new(p);
 
         public DatosModel()
@@ -33,8 +33,8 @@ namespace QuizzServer.Model
         public Dictionary<Seccion, List<Preguntas>> GetSeccion()
         {
             Dictionary<Seccion, List<Preguntas>> response = new();
-            List<Seccion>seccions = new List<Seccion>();
-            List<Preguntas>preguntas = new List<Preguntas>();
+            List<Seccion> seccions = new List<Seccion>();
+            List<Preguntas> preguntas = new List<Preguntas>();
             //regresa todas la tabla de seccion
             foreach (var item in seccions)
             {
@@ -42,7 +42,7 @@ namespace QuizzServer.Model
             }
             return response;
         }
-        public Dictionary<Preguntas,List<Respuestas>> GetPreguntas()
+        public Dictionary<Preguntas, List<Respuestas>> GetPreguntas()
         {
             Dictionary<Preguntas, List<Respuestas>> response = new();
             List<Preguntas> preguntas = conn.Table<Preguntas>().ToList();
@@ -54,46 +54,414 @@ namespace QuizzServer.Model
             }
             return response;
         }
-       
-        /// <summary>
-        /// crea las preguntas y respuestas y las inserta en las tablas
-        /// </summary>
+
+        //public void setSecciones()
+        //{
+        //    List<Seccion> Lsecciones = new List<Seccion>();
+        //    Lsecciones.Add(new Seccion()
+        //    {
+        //        NombreSec = "Ciencias Naturales"
+        //    });
+        //}
         public void SetPreguntas()
         {
             List<Preguntas> Lpreguntas = new List<Preguntas>();
 
-            for (int a = 1; a <= 4; a++)
+
+            List<Respuestas> respuestas = new();
+
+            ///Ciencias naturales Q&A
+
+            Preguntas primeraPreg = new Preguntas()
             {
-                List<Respuestas> respuestas = new();
+                Id = 1,
+                Pregunta = "¿Cuál fisico descubrio la gravedad?",
+                IdSeccion = 1,
+                RespCorrecta = 1,
 
-                Preguntas primeraPreg = new Preguntas()
+
+
+            };
+            for (int i = 1; i <= 4; i++)
+            {
+                switch (i)
                 {
-                    Id = a,
-                    Pregunta = $"Pregunta {a}"
-
-
-                };
-                for (int i = 1; i <= 4; i++)
-                {
-
-
-                    respuestas.Add(new Respuestas()
-                    {
-                        Id = i,
-                        Respuesta = $"Respuesta {i}",
-                        IdPregunta = primeraPreg.Id
-                    });
+                    case 1:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Isaac Newton",
+                            IdPregunta = primeraPreg.Id
+                        });
+                        break;
+                    case 2:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Galileo Galilei",
+                            IdPregunta = primeraPreg.Id
+                        });
+                        break;
+                    case 3:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Niels Bohr",
+                            IdPregunta = primeraPreg.Id
+                        });
+                        break;
+                    case 4:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Albert Einstein",
+                            IdPregunta = primeraPreg.Id
+                        });
+                        break;
 
                 }
-                Lpreguntas.Add(new Preguntas()
-                {
-                    Id= primeraPreg.Id,
-                    Pregunta = primeraPreg.Pregunta,
-                    //respuestas = new List<Respuestas>(primeraPreg.respuestas)
-                    
-                });;
-                conn.InsertAll(respuestas);
+
+
             }
+            Lpreguntas.Add(new Preguntas()
+            {
+                Id = primeraPreg.Id,
+                Pregunta = primeraPreg.Pregunta,
+                //respuestas = new List<Respuestas>(primeraPreg.respuestas)
+
+            }); ;
+            Preguntas Pregu2 = new Preguntas()
+            {
+                Id = 2,
+                IdSeccion = 1,
+                Pregunta = "¿Cuál es el planeta más grande de nuestro sistema solar?",
+                RespCorrecta = 3
+            };
+            for (int i = 1; i <= 4; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Saturno",
+                            IdPregunta = Pregu2.Id
+                        });
+                        break;
+                    case 2:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Marte",
+                            IdPregunta = Pregu2.Id
+                        });
+                        break;
+                    case 3:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Jupiter",
+                            IdPregunta = Pregu2.Id
+                        });
+                        break;
+                    case 4:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Neptuno",
+                            IdPregunta = Pregu2.Id
+                        });
+                        break;
+
+                }
+
+
+            }
+            Lpreguntas.Add(new Preguntas()
+            {
+                Id = Pregu2.Id,
+                Pregunta = Pregu2.Pregunta,
+
+            }); ;
+            Preguntas Pregu3 = new Preguntas()
+            {
+                Id = 3,
+                IdSeccion = 1,
+                Pregunta = "¿Cuál es el símbolo químico del agua?",
+                RespCorrecta = 3
+            };
+            for (int i = 1; i <= 4; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"F",
+                            IdPregunta = Pregu3.Id
+                        });
+                        break;
+                    case 2:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"HO2",
+                            IdPregunta = Pregu3.Id
+                        });
+                        break;
+                    case 3:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"K",
+                            IdPregunta = Pregu3.Id
+                        });
+                        break;
+                    case 4:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"H2O",
+                            IdPregunta = Pregu3.Id
+                        });
+                        break;
+
+                }
+
+
+            }
+            Lpreguntas.Add(new Preguntas()
+            {
+                Id = Pregu3.Id,
+                Pregunta = Pregu3.Pregunta,
+
+            }); ;
+            Preguntas Pregu4 = new Preguntas()
+            {
+                Id = 4,
+                IdSeccion = 1,
+                Pregunta = "¿Cuál es el símbolo químico del oro?",
+                RespCorrecta = 2
+            };
+            for (int i = 1; i <= 4; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Pb",
+                            IdPregunta = Pregu4.Id
+                        });
+                        break;
+                    case 2:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Au",
+                            IdPregunta = Pregu4.Id
+                        });
+                        break;
+                    case 3:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Ag",
+                            IdPregunta = Pregu4.Id
+                        });
+                        break;
+                    case 4:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Mn",
+                            IdPregunta = Pregu4.Id
+                        });
+                        break;
+
+                }
+
+
+            }
+            Lpreguntas.Add(new Preguntas()
+            {
+                Id = Pregu4.Id,
+                Pregunta = Pregu4.Pregunta,
+
+            }); ;
+            Preguntas Pregu5 = new Preguntas()
+            {
+                Id = 5,
+                IdSeccion = 1,
+                Pregunta = "¿Cuál es la fórmula química de la sal de mesa?",
+                RespCorrecta = 3
+            };
+            for (int i = 1; i <= 4; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"MgSO4",
+                            IdPregunta = Pregu5.Id
+                        });
+                        break;
+                    case 2:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"CaCO3",
+                            IdPregunta = Pregu5.Id
+                        });
+                        break;
+                    case 3:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"NaCl",
+                            IdPregunta = Pregu5.Id
+                        });
+                        break;
+                    case 4:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"KCl",
+                            IdPregunta = Pregu5.Id
+                        });
+                        break;
+
+                }
+
+
+            }
+            Lpreguntas.Add(new Preguntas()
+            {
+                Id = Pregu5.Id,
+                Pregunta = Pregu5.Pregunta,
+
+            }); ;
+            Preguntas Pregu6 = new Preguntas()
+            {
+                Id = 6,
+                IdSeccion = 1,
+                Pregunta = "¿Cuál es la ciencia que estudia los cuerpos celestes del universo?",
+                RespCorrecta = 2
+            };
+            for (int i = 1; i <= 4; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Astrología",
+                            IdPregunta = Pregu6.Id
+                        });
+                        break;
+                    case 2:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Astronomía",
+                            IdPregunta = Pregu6.Id
+                        });
+                        break;
+                    case 3:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Elogía",
+                            IdPregunta = Pregu6.Id
+                        });
+                        break;
+                    case 4:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"Meteorología",
+                            IdPregunta = Pregu6.Id
+                        });
+                        break;
+
+                }
+
+
+            }
+            Lpreguntas.Add(new Preguntas()
+            {
+                Id = Pregu6.Id,
+                Pregunta = Pregu6.Pregunta,
+
+            }); ;
+
+            ///Historia Q&A
+            Preguntas PHistoria1 = new Preguntas()
+            {
+                Id = 7,
+                Pregunta = "¿En que fecha se firmo la constitucion de 1917?",
+                IdSeccion = 2,
+                RespCorrecta = 1,
+
+
+
+            };
+            for (int i = 1; i <= 4; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"5 de mayo de 1917",
+                            IdPregunta = PHistoria1.Id
+                        });
+                        break;
+                    case 2:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"5 de febrero de 1917",
+                            IdPregunta = PHistoria1.Id
+                        });
+                        break;
+                    case 3:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"5 de febrero de 1920",
+                            IdPregunta = PHistoria1.Id
+                        });
+                        break;
+                    case 4:
+                        respuestas.Add(new Respuestas()
+                        {
+                            Id = i,
+                            Respuesta = $"5 de mayo de 1920",
+                            IdPregunta = PHistoria1.Id
+                        });
+                        break;
+
+                }
+
+
+            }
+            Lpreguntas.Add(new Preguntas()
+            {
+                Id = PHistoria1.Id,
+                Pregunta = PHistoria1.Pregunta,
+
+            }); ;
+
+            conn.InsertAll(respuestas);
+
             conn.InsertAll(Lpreguntas);
         }
     }
--- Quizz/Model/Preguntas.cs
+++ Quizz/Model/Preguntas.cs
@@ -10,21 +10,23 @@ namespace QuizzServer.Model
 
     public class Seccion
     {
+        [AutoIncrement]
         public int Id { get; set; }
         public string NombreSec { get; set; } = null!;
     }
     public class Preguntas
     {
-        [PrimaryKey]
+        [AutoIncrement]
         public int Id { get; set; }
         public string Pregunta { get; set; } = null!;
+        public int RespCorrecta { get; set; }
         public int IdSeccion { get; set; }
         //public List<Respuestas>respuestas { get; set; } = new List<Respuestas>();
 
     }
     public class Respuestas
     {
-    
+        
         public int Id { get; set; }
         public string Respuesta { get; set; } = null!; 
         public int IdPregunta { get; set; }
